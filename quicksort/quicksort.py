26b3d36f5e05d4ca2f1f4b6dfbad90c0b5a31c30
nealdotpy <nealpatel2021@gmail.com>
2021-02-18 14:38:35 -0500

    implemented non-pythonic and pythonic quicksort-partition

--- quicksort/quicksort.py
+++ quicksort/quicksort.py
@@ -9,30 +9,53 @@ class Array:
 
     def sort(self) -> None:
         # mimicks the List's inplace timsort function
-        self.quicksort(0, self.length)
+        self.quicksort(0, self.length - 0)
 
 
     def quicksort(self, lo: int, hi: int) -> bool:
 
-        def partition(lo: int, hi: int) -> int:
-            res = self.res # "alias"
-            pivot = res[lo]
-            i, j = lo, hi
-            while i < j:
-                while i < pivot:
-                    i += 1
-                while j > pivot:
-                    j += 1
-                res[i], res[j] = res[j], res[i] 
-            res[j], pivot = pivot, res[j]
-            return j
-
-        while lo < hi:
-            pivot_index = partition(lo, hi)
+        if lo < hi:
+            pivot_index = self.partition_pythonic(lo, hi)
             self.quicksort(lo, pivot_index)
             self.quicksort(pivot_index + 1, hi)
 
         return True
+
+    def partition_non_pythonic(self, lo: int, hi: int) -> int:
+        pivot = self.res[lo]
+        i, j = lo, hi
+        while i < j:
+            while 1:
+                i += 1
+                if i >= self.length:
+                    break 
+                if self.res[i] > pivot:
+                    break
+            while 1:
+                j -= 1
+                if j < 0:
+                    break
+                if self.res[j] <= pivot:
+                    break
+            if i < j:
+                self.res[i], self.res[j] = self.res[j], self.res[i]
+        self.res[j], self.res[lo] = self.res[lo], self.res[j]
+        return j
+
+    def partition_pythonic(self, lo: int, hi: int) -> int:
+        pivot = self.res[lo]
+        i, j = lo, hi
+        while i < j:
+            i, j = i + 1, j - 1
+            while i < self.length and self.res[i] <= pivot:
+                i += 1
+            while j >= 0 and self.res[j] > pivot:
+                j -= 1
+            if i < j:
+                self.res[i], self.res[j] = self.res[j], self.res[i]
+        self.res[j], self.res[lo] = self.res[lo], self.res[j]
+        return j
+
 
     def __str__(self):
         return f'{self.res}'
@@ -40,7 +63,12 @@ class Array:
 
 if __name__ == "__main__":
 
-    A = Array([5, 3, 2, 0, 1, 3, 4, 5, 6, 11, 34, 20, 12])
-    print(f'unsorted: {A}')
-    A.sort()
-    print(f'sorted: {A}')
+    A = Array([5, 3, 2, 0, 1, 25, 21, 18, 8])
+    D = Array([5, 3, 2, 0, 1, 3, 4, 5, 6, 11, 34, 20, 12])
+
+    test_cases = [A, D]
+
+    for case in test_cases:
+        print(f'unsorted: {case} ({case.length})')
+        case.sort()
+        print(f'sorted: {case}')
