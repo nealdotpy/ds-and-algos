20cba86e6cb8c5204b29974c358a879eb0928a1b
nealdotpy <nealpatel2021@gmail.com>
2021-02-21 14:46:05 -0500

    pesk pointers!

--- sort/quicksort.cpp
+++ sort/quicksort.cpp
@@ -5,18 +5,10 @@
 
 // using namespace std;
 
-int partition(int* array, int lo, int hi) {
-	int pivot = array[lo];
-	return 0;
-}
-
-int quicksort(int* array, int lo, int hi) {
-	if (lo < hi) {
-		int pivotIndex = partition(array, lo, hi);
-		quicksort(array, lo, pivotIndex);
-		quicksort(array, pivotIndex + 1, hi);
-	}
-	return 1;
+void swap(int* a, int* b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
 void printArray(std::vector<int> array) {
@@ -26,6 +18,37 @@ void printArray(std::vector<int> array) {
 	std::cout << std::endl;
 }
 
+int partition(std::vector<int>* array, int lo, int hi) {
+	int pivot = (*array)[lo];
+	int i = lo;
+	int j = hi;
+	while (i < j) {
+		do {
+			i++;
+		} while ((*array)[i] < pivot);
+		do {
+			j--;
+		} while ((*array)[j] > pivot);
+		if (i < j) {
+			swap(&(*array)[i], &(*array)[j]);
+		}
+	}
+	swap(&(*array)[j], &(*array)[lo]);
+	return j;
+}
+
+void quicksort(std::vector<int>* array, int lo, int hi) {
+	if (lo < hi) {
+		int pivotIndex = partition(array, lo, hi);
+		quicksort(array, lo, pivotIndex);
+		quicksort(array, pivotIndex + 1, hi);
+	}
+}
+
+// void test(std::vector<int>* vec) {
+// 	(*vec)[0] = -1;
+// }
+
 int main() {
 
 	std::vector<std::vector<int>> testCases {
@@ -34,10 +57,14 @@ int main() {
 		{20, 94, 28, 8, 34, 64, 0, 2, 7, 49, 4, 7, 5, 2, 98}
 	};
 
+	// std::cout << testCases[0][0] << std::endl;
+	// test(&testCases[0]);
+	// std::cout << testCases[0][0] << std::endl;
+
 	for (auto tCase: testCases) {
 		std::cout << "sorting: ";
 		printArray(tCase);
-		// quicksort(tCase);
+		quicksort(&tCase, 0, tCase.size());
 		std::cout << "sorted: ";
 		printArray(tCase);
 	}
